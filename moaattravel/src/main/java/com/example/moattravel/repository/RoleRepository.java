18ece181fb42e6892e59c06d4af51836f98b6e02
Yoshiyuki <your-yoshi=baseball_yuki@yahoo.co.jp>
2026-05-18 14:55:41 +0900

    ログイン機能の実装

---
+++ moaattravel/src/main/java/com/example/moattravel/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.moattravel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.moattravel.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role,Integer> {
+
+}
